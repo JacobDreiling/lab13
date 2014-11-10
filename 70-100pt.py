3a8afe23364217a27b6616587ff3ea9813a2b90d
JacobDreiling <quasarbooster@gmail.com>
2014-11-10 15:13:36 -0600

    70pt donzowd

--- 70-100pt.py
+++ 70-100pt.py
@@ -13,10 +13,11 @@ from Tkinter import *
 root = Tk()
 
 drawpad = Canvas(root, width=800,height=600, background='white')
-player = drawpad.create_oval(390,580,410,600, fill="red")
+player = drawpad.create_rectangle(390,580,410,600, fill="red")
 
 # Create your "enemies" here, before the class
-
+enemyEins = drawpad.create_oval(50,50,119,188, fill="purple")
+enemyZwei = drawpad.create_oval(50,200,188,269, fill="blue")
 
 class MyApp:
 	def __init__(self, parent):
@@ -25,10 +26,25 @@ class MyApp:
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
        	    self.up = Button(self.myContainer1)
-       	    self.up.configure(text="up", background= "green")
-       	    self.up.grid(row=0,column=0)
+       	    self.up.configure(text="up", background= "red")
+       	    self.up.grid(row=0,column=1)
        	    # Bind an event to the first button
        	    self.up.bind("<Button-1>", self.upClicked)
+       	    
+       	    self.unter = Button(self.myContainer1)
+       	    self.unter.configure(text="unter", background= "red")
+       	    self.unter.grid(row=2,column=1)
+       	    self.unter.bind("<Button-1>", self.unterClicked)
+       	    
+       	    self.linke = Button(self.myContainer1)
+       	    self.linke.configure(text="linke", background= "red")
+       	    self.linke.grid(row=1,column=0)
+       	    self.linke.bind("<Button-1>", self.linkeClicked)
+       	    
+       	    self.recht = Button(self.myContainer1)
+       	    self.recht.configure(text="recht", background= "red")
+       	    self.recht.grid(row=1,column=2)
+       	    self.recht.bind("<Button-1>", self.rechtClicked)
        	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=RIGHT)
@@ -38,6 +54,8 @@ class MyApp:
 	def animate(self):
 	    global drawpad
 	    global player
+	    global enemyEins
+	    global enemyZwei
 	    # Remember to include your "enemies" with "global"
 	    
 	    # Uncomment this when you're ready to test out your animation!
@@ -47,6 +65,21 @@ class MyApp:
 	   global oval
 	   global player
 	   drawpad.move(player,0,-20)
+	   
+	def unterClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,0,20)
+	   
+	def linkeClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,-20,0)
+	   
+	def rechtClicked(self, event):   
+	   global oval
+	   global player
+	   drawpad.move(player,20,0)
 		
 
 app = MyApp(root)
