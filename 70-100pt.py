59c27154894f1ca5e30f4dfd5edc34fa457b1713
JacobDreiling <quasarbooster@gmail.com>
2014-11-10 16:07:51 -0600

    100pt incomplete

--- 70-100pt.py
+++ 70-100pt.py
@@ -18,6 +18,7 @@ player = drawpad.create_rectangle(390,580,410,600, fill="red")
 # Create your "enemies" here, before the class
 enemyEins = drawpad.create_oval(50,50,119,188, fill="purple")
 enemyZwei = drawpad.create_oval(50,200,188,269, fill="blue")
+enemyDrei = drawpad.create_oval(50,300,188,438, fill="green")
 
 class MyApp:
 	def __init__(self, parent):
@@ -25,11 +26,11 @@ class MyApp:
        	    self.myParent = parent  
        	    self.myContainer1 = Frame(parent)
        	    self.myContainer1.pack()
-       	    self.up = Button(self.myContainer1)
-       	    self.up.configure(text="up", background= "red")
-       	    self.up.grid(row=0,column=1)
+       	    self.ueber = Button(self.myContainer1)
+       	    self.ueber.configure(text="ueber", background= "red")
+       	    self.ueber.grid(row=0,column=1)
        	    # Bind an event to the first button
-       	    self.up.bind("<Button-1>", self.upClicked)
+       	    self.ueber.bind("<Button-1>", self.ueberClicked)
        	    
        	    self.unter = Button(self.myContainer1)
        	    self.unter.configure(text="unter", background= "red")
@@ -56,30 +57,51 @@ class MyApp:
 	    global player
 	    global enemyEins
 	    global enemyZwei
+	    global enemyDrei
 	    # Remember to include your "enemies" with "global"
-	    
-	    # Uncomment this when you're ready to test out your animation!
-	    #drawpad.after(10,self.animate)
+	    direction = 1
+	    Ex1, Ey1, Ex2, Ey2 = drawpad.coords(enemyEins)
+            if Ex2 > int(drawpad.winfo_width())+69: 
+                direction = -869
+            elif Ex1 < 0:
+                direction = 690
+            drawpad.move(enemyEins,direction,0)
+            
+            Zx1, Zy1, Zx2, Zy2 = drawpad.coords(enemyZwei)
+            if Zx2 > int(drawpad.winfo_width())+138: 
+                direction = -938
+            elif Zx1 < 0:
+                direction = 69
+            drawpad.move(enemyZwei,direction,0)
+            
+            Dx1, Dy1, Dx2, Dy2 = drawpad.coords(enemyDrei)
+            if Dx2 > int(drawpad.winfo_width())+138: 
+                direction = -938
+            elif Dx1 < 0:
+                direction = 6.9
+            drawpad.move(enemyDrei,direction,0)
+            
+	    drawpad.after(1,self.animate)
 		
-	def upClicked(self, event):   
-	   global oval
-	   global player
-	   drawpad.move(player,0,-20)
+	def ueberClicked(self, event):   
+	    global oval
+	    global player
+	    drawpad.move(player,0,-20)
 	   
 	def unterClicked(self, event):   
-	   global oval
-	   global player
-	   drawpad.move(player,0,20)
+	    global oval
+	    global player
+	    drawpad.move(player,0,20)
 	   
 	def linkeClicked(self, event):   
-	   global oval
-	   global player
-	   drawpad.move(player,-20,0)
+	    global oval
+	    global player
+	    drawpad.move(player,-20,0)
 	   
 	def rechtClicked(self, event):   
-	   global oval
-	   global player
-	   drawpad.move(player,20,0)
+	    global oval
+	    global player
+	    drawpad.move(player,20,0)
 		
 
 app = MyApp(root)
