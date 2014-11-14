4796ad33d3ef0b8a17b39653de41f4f8f4e75ef1
JacobDreiling <quasarbooster@gmail.com>
2014-11-14 15:19:48 -0600

    100pt Donion rAngs

--- 70-100pt.py
+++ 70-100pt.py
@@ -14,6 +14,9 @@ root = Tk()
 
 drawpad = Canvas(root, width=800,height=600, background='white')
 player = drawpad.create_rectangle(390,580,410,600, fill="red")
+directionE = 1
+directionZ = 1
+directionD = 1
 
 # Create your "enemies" here, before the class
 enemyEins = drawpad.create_oval(50,50,119,188, fill="purple")
@@ -49,41 +52,8 @@ class MyApp:
        	    
        	    # No need to edit this - just includes the drawpad into our frame
        	    drawpad.pack(side=RIGHT)
-       	    # call the animate function to start our recursion
-       	    self.animate()
-	
-	def animate(self):
-	    global drawpad
-	    global player
-	    global enemyEins
-	    global enemyZwei
-	    global enemyDrei
-	    # Remember to include your "enemies" with "global"
-	    direction = 1
-	    Ex1, Ey1, Ex2, Ey2 = drawpad.coords(enemyEins)
-            if Ex2 > int(drawpad.winfo_width())+69: 
-                direction = -869
-            elif Ex1 < 0:
-                direction = 690
-            drawpad.move(enemyEins,direction,0)
-            
-            Zx1, Zy1, Zx2, Zy2 = drawpad.coords(enemyZwei)
-            if Zx2 > int(drawpad.winfo_width())+138: 
-                direction = -938
-            elif Zx1 < 0:
-                direction = 69
-            drawpad.move(enemyZwei,direction,0)
-            
-            Dx1, Dy1, Dx2, Dy2 = drawpad.coords(enemyDrei)
-            if Dx2 > int(drawpad.winfo_width())+138: 
-                direction = -938
-            elif Dx1 < 0:
-                direction = 6.9
-            drawpad.move(enemyDrei,direction,0)
-            
-	    drawpad.after(1,self.animate)
-		
-	def ueberClicked(self, event):   
+       	    
+       	def ueberClicked(self, event):   
 	    global oval
 	    global player
 	    drawpad.move(player,0,-20)
@@ -102,7 +72,57 @@ class MyApp:
 	    global oval
 	    global player
 	    drawpad.move(player,20,0)
+	
+def animateE():
+	global drawpad
+	global player
+	global enemyEins
+	global enemyZwei
+	global enemyDrei
+	global directionE
+	# Remember to include your "enemies" with "global"
+	Ex1, Ey1, Ex2, Ey2 = drawpad.coords(enemyEins)
+        if Ex2 > int(drawpad.winfo_width())+69: 
+            directionE = -869
+        elif Ex1 < 0:
+            directionE = 690
+        drawpad.move(enemyEins,directionE,0)
+        drawpad.after(20,animateE)
+            
+def animateZ():
+	global drawpad
+	global player
+	global enemyEins
+	global enemyZwei
+	global enemyDrei
+	global directionZ
+        Zx1, Zy1, Zx2, Zy2 = drawpad.coords(enemyZwei)
+        if Zx2 > int(drawpad.winfo_width())+138: 
+            directionZ = -938
+        elif Zx1 < 0:
+            directionZ = 69
+        drawpad.move(enemyZwei,directionZ,0)
+        drawpad.after(20,animateZ)
+            	    
+def animateD():
+	global drawpad
+	global player
+	global enemyEins
+	global enemyZwei
+	global enemyDrei
+	global directionD
+        Dx1, Dy1, Dx2, Dy2 = drawpad.coords(enemyDrei)
+        if Dx2 > int(drawpad.winfo_width())+138: 
+            directionD = -938
+        elif Dx1 < 0:
+            directionD = 6.9
+        drawpad.move(enemyDrei,directionD,0)
+	drawpad.after(20,animateD)
 		
+# call the animate function to start our recursion
+animateE()
+animateZ()
+animateD()
 
 app = MyApp(root)
 root.mainloop()
